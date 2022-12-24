79c06114c0caa03269367050c80fa4ea997d5555
EbraheemAli98 <120432821+EbraheemAli98@users.noreply.github.com>
2022-12-24 03:19:40 +0200

    Delete Src/MCAL/GPT directory

--- Src/MCAL/GPT/Gpt_Lcfg.h
+++
@@ -1,9 +0,0 @@
-#ifndef GPT_LCFG_H
-#define GRT_LCFG_H
-
-/*************************************************************************************************
- *  GLOBAL DATA 
- ************************************************************************************************/
-extern const Gpt_ConfigType Gpt_ChannelConfigurationStr;
-
-#endif  /* GPT_LCFG_H */
